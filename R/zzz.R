0e166762442c4542308ac7827b26adb57502ee08
Dominik Schienstock <d.schienstock@student.unimelb.edu.au>
2022-08-18 17:42:39 +1000

    misc

--- R/zzz.R
+++ R/zzz.R
@@ -83,13 +83,13 @@ cciaCondaCreate <- function(envName = "r-cecelia-env", envType = "image",
     pyModules <- pyModules[grepl(pattern = "^(?!#)", x = pyModules, perl = TRUE)]
     
     reticulate::conda_install(envname = envName, packages = pyModules,
-                              pip = TRUE, pip_options = "--user")
+                              pip = TRUE, pip_options = c("--user"))
     
     # install OME bioformats
     if (envType %in% c("image", "image-nogui")) {
       reticulate::conda_install(
         envname = envName, packages = c("bioformats2raw"), channel = "ome",
-        pip = TRUE, pip_options = "--user"
+        pip = TRUE, pip_options = c("--user")
         )
     }
   }
