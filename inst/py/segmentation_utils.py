425d70a4cf8bd2335d6a395e268cb45c5375bc94
schienstockd <schienstockd@student.unimelb.edu.au>
2022-12-20 13:19:20 +1100

    misc

--- inst/py/segmentation_utils.py
+++ inst/py/segmentation_utils.py
@@ -343,7 +343,7 @@ class SegmentationUtils:
         # TODO is this a fair assumption? - No
         # if alg_labels['base'] is not None and alg_labels['base'].max() > 0:
         #   cur_max_labels = alg_labels['base'].max()
-        next_max_labels = [x.max() for x in alg_labels if x is not None and x.max() > 0]
+        next_max_labels = [x.max() for x in alg_labels.values() if x is not None and x.max() > 0]
         
         if len(next_max_labels) > 0:
           cur_max_labels = max(next_max_labels)
