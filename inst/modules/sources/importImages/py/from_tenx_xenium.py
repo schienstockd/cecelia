94e40100a295649fb88a02c875eabbc0f4753fc2
schienstockd <schienstockd@student.unimelb.edu.au>
2023-03-21 12:38:15 +1100

    misc

--- inst/modules/sources/importImages/py/from_tenx_xenium.py
+++ inst/modules/sources/importImages/py/from_tenx_xenium.py
@@ -89,9 +89,9 @@ def run(params):
       # 'CYTIP', 'IL7R', 'LTB', 'TRAC',
       # # Mphage
       # 'APOC1', 'C15orf48', 'C1QA', 'C1QC', 'CD14',
-      # 'CD163', 'CD68', 'FGL2', 'ITGAX', 'MMP12',
+      'CD163', 'CD68', 'FGL2', 'ITGAX', 'MMP12',
       # # DC
-      # 'CCR7', 'CD83', 'IL3RA', 'LILRA4', 'PLD4',
+      'CCR7', 'CD83', 'IL3RA', 'LILRA4', 'PLD4',
       # # Stroma
       # 'ALDH1A3', 'GJB2', 'LUM', 'MMP2', 'POSTN', 'SFRP4'
       # FOR BRAIN
