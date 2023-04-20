25add0a0d360ce2742c4fef5562dcbd404678f69
schienstockd <schienstockd@student.unimelb.edu.au>
2023-04-20 17:23:25 +1000

    misc

--- inst/app/cecelia-macOSX.sh
+++ inst/app/cecelia-macOSX.sh
@@ -15,6 +15,9 @@ CONDA_ENV=r-cecelia-env
 CECELIA_DIR="$(cd "$(dirname "$0")/.." > /dev/null 2>&1 || exit; pwd -P)"
 R_CALL="library(cecelia);cciaUse('$CECELIA_DIR',initJupyter=TRUE);cciaRunApp(port=6860,launch.browser=TRUE)"
 
+# OMP: Error #15: Initializing libiomp5.dylib, but found libomp.dylib already initialized.
+# https://github.com/dmlc/xgboost/issues/1715#issuecomment-438924721
+export KMP_DUPLICATE_LIB_OK=TRUE
 echo $R_CALL
 
 # init conda
