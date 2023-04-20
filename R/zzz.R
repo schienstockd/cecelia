0261f41ef639d7a57b27c19a1aa390fdbf64995e
schienstockd <schienstockd@student.unimelb.edu.au>
2023-04-20 17:38:08 +1000

    misc

--- R/zzz.R
+++ R/zzz.R
@@ -136,7 +136,8 @@ cciaCondaCreate <- function(envName = "r-cecelia-env", envType = "image",
     reticulate::conda_install(
       envname = envName, packages = c("bioformats2raw"), channel = "ome")
     reticulate::conda_install(
-      envname = envName, packages = c("bftools"), channel = "bioconda")
+      # envname = envName, packages = c("bftools"), channel = "bioconda")
+      envname = envName, packages = c("bftools<6.7.0"), channel = "bioconda")
   }
 }
 
