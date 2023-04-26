b4db225d7f03979f30afbe5ff660bd3d71b0b7c9
schienstockd <schienstockd@student.unimelb.edu.au>
2023-04-26 13:49:54 +1000

    misc

--- inst/py/segmentation_utils.py
+++ inst/py/segmentation_utils.py
@@ -346,7 +346,7 @@ class SegmentationUtils:
               # TODO merge masks - is there a better way?
               labels[j][cur_slices] = np.amax(np.concatenate(
                 label_utils.match_masks(
-                  [y, labels[j][cur_slices]],
+                  [y, np.squeeze(labels[j][cur_slices]]),
                   # [labels[j][cur_slices], y],
                   stitch_threshold = self.label_overlap,
                   remove_unmatched = False
