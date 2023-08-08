7727c823379e992cb786e9e95bfb185aac72b00c
schienstockd <schienstockd@student.unimelb.edu.au>
2023-08-08 14:09:48 +1000

    misc

--- inst/py/label_utils.py
+++ inst/py/label_utils.py
@@ -4,8 +4,8 @@ import numpy as np
 Adapted from cellpose.metrics to find intersection between masks
 https://github.com/MouseLand/cellpose/blob/4e8205125750c0c82e03386f28ff6d4bef1da6c7/cellpose/metrics.py#L168
 """
-def intersection_over_union(x, y, dtype = np.uint16, logfile_utils = None):
-  overlap = label_overlap(x, y, dtype = dtype, logfile_utils = logfile_utils)
+def intersection_over_union(x, y, dtype = np.uint16):
+  overlap = label_overlap(x, y, dtype = dtype)
   n_pixels_x = np.sum(overlap, axis = 0, keepdims = True)
   n_pixels_true = np.sum(overlap, axis = 1, keepdims = True)
   iou = overlap / (n_pixels_x + n_pixels_true - overlap)
@@ -17,7 +17,7 @@ def intersection_over_union(x, y, dtype = np.uint16, logfile_utils = None):
 Adapted from cellpose.metrics to find overlap between masks
 https://github.com/MouseLand/cellpose/blob/4e8205125750c0c82e03386f28ff6d4bef1da6c7/cellpose/metrics.py#L133
 """
-def label_overlap(x, y, dtype = np.uint16, logfile_utils = None):
+def label_overlap(x, y, dtype = np.uint16):
   # put label arrays into standard form then flatten them 
   x = x.ravel()
   y = y.ravel()
@@ -25,11 +25,6 @@ def label_overlap(x, y, dtype = np.uint16, logfile_utils = None):
   # preallocate a 'contact map' matrix
   # TODO can you .. incorporate min?
   overlap = np.zeros((1 + x.max(), 1 + y.max()), dtype = dtype)
-  
-  if logfile_utils is not None:
-    logfile_utils.log('>> overlap')
-    logfile_utils.log(f'> MAX {x.max()}:{y.max()}')
-    logfile_utils.log_mem_usage(context = locals())
   
   # loop over the labels in x and add to the corresponding
   # overlap entry. If label A in x and label B in y share P
@@ -45,7 +40,7 @@ Match masks
 adapted from cellpose.utils.stitch3D
 https://github.com/MouseLand/cellpose/blob/4e8205125750c0c82e03386f28ff6d4bef1da6c7/cellpose/utils.py#L353
 """
-def match_masks(masks, stitch_threshold = 0.2, remove_unmatched = False, dtype = None, logfile_utils = None):
+def match_masks(masks, stitch_threshold = 0.2, remove_unmatched = False, dtype = None):
   # save merged labels
   mmax = np.max([x.max() for x in masks])
   mmin = np.min([x[x > 0].min() - 1 if np.any(x) else 0 for x in masks])
@@ -54,12 +49,6 @@ def match_masks(masks, stitch_threshold = 0.2, remove_unmatched = False, dtype =
   # preserve dtype - use first image as reference
   if dtype is None:
     dtype = masks[0].dtype
-  
-  if logfile_utils is not None:
-    logfile_utils.log([np.unique(x) for x in masks])
-    logfile_utils.log([x[x > 0].min() for x in masks if np.any(x)])
-    logfile_utils.log([x[x > 0].min() - 1 if np.any(x) else 0 for x in masks])
-    logfile_utils.log(f'>> mmin {mmin}')
   
   # TODO adjust label number to keep computation low
   # Maybe a sparse matrix would be better .. ?
@@ -72,7 +61,7 @@ def match_masks(masks, stitch_threshold = 0.2, remove_unmatched = False, dtype =
       masks[i] = (masks[i + 1] > 0) * masks[i]
     
     # get intersection
-    iou = intersection_over_union(masks[i + 1], masks[i], logfile_utils = logfile_utils)[1:, 1:]
+    iou = intersection_over_union(masks[i + 1], masks[i])[1:, 1:]
     
     if not iou.size and empty == 0:
       masks[i + 1] = masks[i + 1]
