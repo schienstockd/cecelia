e90f29423883de3a38d6f7d4a773b8f2f4c0bc73
schienstockd <schienstockd@student.unimelb.edu.au>
2023-08-08 10:39:24 +1000

    misc

--- inst/py/segmentation_utils.py
+++ inst/py/segmentation_utils.py
@@ -355,8 +355,11 @@ class SegmentationUtils:
             # merge with exisiting labels
             if self.label_overlap > 0:
               self.logfile_utils.log(f'> Merge {j} labels by overlap {self.label_overlap}')
-              self.logfile_utils.log(sys.getsizeof(labels[j][label_slices]))
-              self.logfile_utils.log(sys.getsizeof(alg_labels[j]))
+              
+              self.logfile_utils.log_mem_usage(var_names = {
+                'labels': labels[j][label_slices],
+                'alg_labels': alg_labels[j]
+              })
               
               # get matches
               matched_masks = label_utils.match_masks(
@@ -365,16 +368,20 @@ class SegmentationUtils:
                 remove_unmatched = False)
               
               self.logfile_utils.log(f'> After {j}')
-              self.logfile_utils.log(sys.getsizeof(labels[j][label_slices]))
-              self.logfile_utils.log(sys.getsizeof(alg_labels[j]))
-              self.logfile_utils.log(sys.getsizeof(matched_masks[0]))
-              self.logfile_utils.log(sys.getsizeof(matched_masks[1]))
+              self.logfile_utils.log_mem_usage(var_names = {
+                'labels': labels[j][label_slices],
+                'alg_labels': alg_labels[j],
+                'matched_masks 1': matched_masks[0],
+                'matched_masks 2': matched_masks[1]
+              })
               
               # TODO merge masks - is there a better way?
               labels[j][label_slices] = np.maximum(matched_masks[0], matched_masks[1])
               
               self.logfile_utils.log(f'> Copied {j}')
-              self.logfile_utils.log(sys.getsizeof(labels[j][label_slices]))
+              self.logfile_utils.log_mem_usage(var_names = {
+                'labels': labels[j][label_slices]
+              })
             else:
               self.logfile_utils.log(f'> Merge {j} labels by maximum')
               # this will lead to artefacts - but is fast
