043003e69ea136216bf16c3d7da0e5e392e82ef4
schienstockd <schienstockd@student.unimelb.edu.au>
2023-08-09 19:52:29 +1000

    misc

--- inst/py/label_utils.py
+++ inst/py/label_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.sparse import coo_array
+from sklearn import preprocessing
 
 """
 Adapted from cellpose.metrics to find intersection between masks
@@ -9,11 +10,11 @@ def intersection_over_union(x, y, dtype = np.uint16):
   # get overlap
   overlap = label_overlap(x, y, dtype = dtype)
   
-  n_pixels_x = np.sum(overlap, axis = 0, keepdims = True, dtype = np.uint16)
-  n_pixels_true = np.sum(overlap, axis = 1, keepdims = True, dtype = np.uint16)
-  n_compose = (n_pixels_x + n_pixels_true - overlap)
-  iou = np.divide(overlap, n_compose, dtype = np.float16, where = n_compose != 0)
-  iou[np.isnan(iou)] = 0.0
+  # n_pixels_x = np.sum(overlap, axis = 0, keepdims = True, dtype = np.uint16)
+  # n_pixels_true = np.sum(overlap, axis = 1, keepdims = True, dtype = np.uint16)
+  # n_compose = (n_pixels_x + n_pixels_true - overlap)
+  # iou = np.divide(overlap, n_compose, dtype = np.float16, where = n_compose != 0)
+  # iou[np.isnan(iou)] = 0.0
   
   # # TODO there must be a better way to use sparse matrices
   # n_pixels_x = overlap.sum(axis = 0).astype(np.uint16)
@@ -27,7 +28,8 @@ def intersection_over_union(x, y, dtype = np.uint16):
   #                 dtype = np.float16, where = n_compose != 0)
   # iou[np.isnan(iou)] = 0.0
   
-  return iou
+  # return iou
+  return preprocessing.normalize(overlap, norm='l1', axis=1)
   
 """
 Adapted from cellpose.metrics to find overlap between masks
@@ -37,21 +39,21 @@ def label_overlap(x, y, dtype = np.uint16):
   # put label arrays into standard form then flatten them 
   x = x.ravel()
   y = y.ravel()
-  # z = [1] * len(x)
+  z = [1] * len(x)
   
   # preallocate a 'contact map' matrix
   # TODO can you make a sparse matrix out of that .. ?
-  overlap = np.zeros((1 + x.max(), 1 + y.max()), dtype = dtype)
+  # overlap = np.zeros((1 + x.max(), 1 + y.max()), dtype = dtype)
   
   # loop over the labels in x and add to the corresponding
   # overlap entry. If label A in x and label B in y share P
   # pixels, then the resulting overlap is P
   # len(x)=len(y), the number of pixels in the whole image 
-  for i in range(len(x)):
-    overlap[x[i],y[i]] += 1
+  # for i in range(len(x)):
+  #   overlap[x[i],y[i]] += 1
   
-  # return coo_array((z, (x, y)), shape = (len(x), len(y)), dtype = dtype)
-  return overlap
+  return coo_array((z, (x, y)), shape = (len(x), len(y)), dtype = dtype)
+  # return overlap
 
 """
 Match masks
@@ -95,9 +97,9 @@ def match_masks(masks, stitch_threshold = 0.2, remove_unmatched = False, dtype =
     #   masks[i + 1] = istitch[masks[i + 1]]
     else:
       iou[iou < stitch_threshold] = 0
-      iou[iou < iou.max(axis = 0)] = 0
+      iou[iou < iou.max(axis = 0).toarray()] = 0
       istitch = iou.argmax(axis = 1) + 1
-      ino = np.nonzero(iou.max(axis = 1) == 0.0)[0]
+      ino = np.nonzero(iou.max(axis = 1).toarray() == 0.0)[0]
       istitch[ino] = np.arange(mmax + 1, mmax + len(ino) + 1, 1, dtype = dtype)
       # mmax += len(ino)
       istitch = np.append(np.array(0), istitch)
