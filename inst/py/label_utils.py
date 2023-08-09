b2faab7ca43ca623e00055757c970a920bf879dc
schienstockd <schienstockd@student.unimelb.edu.au>
2023-08-09 14:26:35 +1000

    misc

--- inst/py/label_utils.py
+++ inst/py/label_utils.py
@@ -129,7 +129,8 @@ def match_masks(masks, stitch_threshold = 0.2, remove_unmatched = False, dtype =
       
   # readjust label numbers
   for i in range(len(masks)):
-    masks[i][masks[i] > 0] = masks[i][masks[i] > 0] + mmin - mmax
+    # masks[i][masks[i] > 0] = masks[i][masks[i] > 0] + mmin - mmax
+    # masks[i][masks[i] > 0] = masks[i][masks[i] > 0] + mmin - mmax
     
   if logfile_utils is not None:
     logfile_utils.log(max([x.max() for x in masks]))
