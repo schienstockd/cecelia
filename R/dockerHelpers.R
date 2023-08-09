467d03ba65009656e04815e8186b73fadd6f6516
schienstockd <schienstockd@student.unimelb.edu.au>
2023-08-09 11:05:20 +1000

    misc

--- R/dockerHelpers.R
+++ R/dockerHelpers.R
@@ -1,12 +1,12 @@
 # map path back to host system
-.dockerMapPathToHost <- function(filePath, path = "projects") {
+.dockerMapPathToHost <- function(filePath) {
   if (cciaConf()$docker$useDocker == TRUE) {
     # replace mapping with defined path
     for (i in length(cciaConf()$docker$pathMapping$from)) {
       filePath <- stringr::str_replace(
         filePath,
-        cciaConf()$docker$pathMapping$from[[path]],
-        cciaConf()$docker$pathMapping$to[[path]])
+        cciaConf()$docker$pathMapping$from[[i]],
+        cciaConf()$docker$pathMapping$to[[i]])
     }
   }
   
