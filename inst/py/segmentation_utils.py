5d836c1ed33c26d9b5bf48c3bcc6d7e63251f946
schienstockd <schienstockd@student.unimelb.edu.au>
2023-08-18 15:28:28 +1000

    misc

--- inst/py/segmentation_utils.py
+++ inst/py/segmentation_utils.py
@@ -262,10 +262,6 @@ class SegmentationUtils:
     else:
       clear_borders = len(slices) > 1
     
-    # DEBUG FOR MULTIPLE LABELS
-    if self.halo_size > 0:
-      slices = slices[0:2]
-    
     # go through slices
     for i, cur_slices in enumerate(slices):
       self.logfile_utils.log('>> Slice: ' + str(i + 1) + '/' + str(len(slices)))
