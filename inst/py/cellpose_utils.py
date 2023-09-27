02f4379bf4fb0ee23de5001f26d99afd0d6742ac
schienstockd <schienstockd@student.unimelb.edu.au>
2023-09-27 16:31:09 +1000

    misc

--- inst/py/cellpose_utils.py
+++ inst/py/cellpose_utils.py
@@ -202,7 +202,7 @@ class CellposeUtils(SegmentationUtils):
       self.logfile_utils.log('> Average time')
       
       # cur_im_dat = np.average(cur_im_dat, axis = t_idx)
-      cur_im_dat = np.maximum(cur_im_dat, axis = t_idx)
+      cur_im_dat = np.max(cur_im_dat, axis = t_idx)
     
     # get label shape
     label_shape = list(cur_im_dat.shape)
