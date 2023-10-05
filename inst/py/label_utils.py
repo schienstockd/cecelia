003157eadf952806de1f4ccba9421e9112673786
schienstockd <schienstockd@student.unimelb.edu.au>
2023-10-05 13:31:02 +1100

    misc

--- inst/py/label_utils.py
+++ inst/py/label_utils.py
@@ -123,7 +123,8 @@ def match_masks(masks, stitch_threshold = 0.0, remove_unmatched = False,
     #   masks[i + 1] = istitch[masks[i + 1]]
     else:
       iou = iou > stitch_threshold
-      x = np.array(iou.argmax(axis = 0))[0,:]
+      # x = np.array(iou.argmax(axis = 0))[0,:]
+      x = np.array(iou.argmax(axis = 0))
       y = np.arange(0, x.size, 1, dtype = dtype)
       z = iou.max(axis = 0).toarray()[0,:]
       iou = coo_array((z, (x, y)), shape = (len(x), len(y)))
